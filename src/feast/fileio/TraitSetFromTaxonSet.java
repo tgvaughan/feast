c476d25f7d6446393e019a555f672c929b4d7836
Tim Vaughan <timv@ughan.xyz>
2020-03-16 23:10:17 +0100

    Added class for extracting trait sets from taxon sets.

---
+++ src/feast/fileio/TraitSetFromTaxonSet.java
@@ -0,0 +1,64 @@
+package feast.fileio;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.evolution.tree.TraitSet;
+
+@Description("Initialize a trait set from a taxon set. " +
+        "Permits similar functionality to the 'autoconfigure' option in BEAUti.")
+public class TraitSetFromTaxonSet extends TraitSet {
+
+    public Input<String> delimiterInput = new Input<>("delimiter",
+            "Delimiter used to split taxon names.",
+            Input.Validate.REQUIRED);
+
+    public Input<Boolean> beforeInput = new Input<>("everythingBeforeFirst",
+            "Use everything before this delimiter.", false);
+
+    public Input<Boolean> afterInput = new Input<>("everythingAfterLast",
+            "Use everything before this delimiter.", false);
+
+    public Input<Integer> groupInput = new Input<>("takeGroup",
+            "Use everthing in this group.", -1);
+
+    String delimiter;
+    boolean before, after;
+    int group;
+
+    public TraitSetFromTaxonSet() {
+        traitsInput.setRule(Input.Validate.OPTIONAL);
+    }
+
+    @Override
+    public void initAndValidate() {
+        delimiter = delimiterInput.get();
+        before = beforeInput.get();
+        after = afterInput.get();
+        group = groupInput.get();
+
+        if (!before  && !after && group < 0) {
+            throw new IllegalArgumentException("At least one of everythingBefore, everythingAfter or takeGroup " +
+                    "must be specified.");
+        }
+
+        StringBuilder traitSB = new StringBuilder();
+
+        for (String taxon : taxaInput.get().asStringList()) {
+            if (traitSB.length()>0)
+                traitSB.append(",");
+            traitSB.append(taxon).append("=");
+
+            if (before) {
+                traitSB.append(taxon, 0, taxon.indexOf(delimiter));
+            } else if (after) {
+                traitSB.append(taxon, taxon.lastIndexOf(delimiter)+1, taxon.length());
+            } else {
+                traitSB.append(taxon.split(delimiter)[group]);
+            }
+        }
+
+        traitsInput.setValue(traitSB.toString(), this);
+
+        super.initAndValidate();
+    }
+}
