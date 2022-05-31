e0da0e67c934e94f618f0cf3d6ad7f99feba02e9
Tim Vaughan <timv@ughan.xyz>
2022-05-31 22:01:05 +0200

    Added TraitSetFromXSV.

---
+++ src/feast/fileio/TraitSetFromXSV.java
@@ -0,0 +1,67 @@
+package feast.fileio;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.evolution.tree.TraitSet;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Description("Initialize a trait set from a CSV/TSV file.")
+public class TraitSetFromXSV extends TraitSet {
+
+    public Input<String> fileNameInput = new Input<>("fileName", "Name of CSV/TSV file to extract values from.",
+            Input.Validate.REQUIRED);
+
+    public Input<String> sepInput = new Input<>("sep",
+            "Separator for CSV/TSV file.  Default is TAB.","\t");
+
+    public Input<Integer> taxonNameColInput = new Input<>("taxonNameCol",
+            "Index of column containing taxon names (Default 0).", 0);
+
+    public Input<Integer> traitValueColInput = new Input<>("traitValueCol",
+            "Index of column containing trait names (Default 1).", 1);
+
+
+    public Input<Boolean> skipHeaderRowInput = new Input<>("skipFirstRow",
+            "If true, skip first row. (Default false.)", false);
+
+    public TraitSetFromXSV() {
+        traitsInput.setRule(Input.Validate.OPTIONAL);
+    }
+
+    @Override
+    public void initAndValidate() {
+
+        Map<String, String> taxonValues = new HashMap<>();
+        StringBuilder traitSB = new StringBuilder();
+
+        String sep = sepInput.get();
+        int keyIdx = taxonNameColInput.get();
+        int valIdx = traitValueColInput.get();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileNameInput.get()))) {
+            if (skipHeaderRowInput.get())
+                reader.readLine();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (traitSB.length()>0)
+                    traitSB.append(",");
+                String[] elements = line.split(sep);
+                traitSB.append(elements[keyIdx]).append("=").append(elements[valIdx]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading from file '"
+                    + fileNameInput.get() + "': " + e.getMessage());
+        }
+
+        traitsInput.setValue(traitSB.toString(), this);
+
+        super.initAndValidate();
+    }
+}
---
+++ test/feast/fileio/TraitSetFromXSVTest.java
@@ -0,0 +1,34 @@
+package feast.fileio;
+
+import beast.evolution.alignment.TaxonSet;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TraitSetFromXSVTest {
+
+    @Test
+    public void test() throws Exception {
+
+        AlignmentFromFasta alignment = new AlignmentFromFasta();
+        alignment.initByName(
+                "fileName", "test/feast/fileio/test_alignment.fasta");
+
+        TraitSetFromXSV traitSet = new TraitSetFromXSV();
+        traitSet.initByName("taxa", new TaxonSet(alignment),
+                "fileName", "test/feast/fileio/test_traitset.csv",
+                "skipFirstRow", true,
+                "sep", ",",
+                "traitname", "date",
+                "dateFormat", "yyyy-M-dd");
+
+        assertEquals(0.6612021857922628,
+                traitSet.getValue("fish"), 1e-10);
+        assertEquals(0.6420765027321522,
+                traitSet.getValue("frog"), 1e-10);
+        assertEquals(0.0,
+                traitSet.getValue("snake"), 1e-10);
+        assertEquals(0.0,
+                traitSet.getValue("mouse"), 1e-10);
+    }
+}
