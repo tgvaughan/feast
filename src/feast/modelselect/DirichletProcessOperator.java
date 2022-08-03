5a16ed4a83c0ab9481a9d5a805469c7d202baceb
Tim Vaughan <timv@ughan.xyz>
2022-08-03 20:39:40 +1000

    Added operator and prior for DPP models.

---
+++ src/feast/modelselect/DirichletProcessOperator.java
@@ -0,0 +1,95 @@
+package feast.modelselect;
+
+import beast.core.Function;
+import beast.core.Input;
+import beast.core.Operator;
+import beast.core.StateNode;
+import beast.core.parameter.RealParameter;
+import beast.math.distributions.ParametricDistribution;
+import beast.util.Randomizer;
+import org.apache.commons.math.MathException;
+
+import java.util.*;
+
+public class DirichletProcessOperator extends Operator {
+
+    public Input<RealParameter> parameterInput = new Input<>(
+            "parameter",
+            "Parameter to operate on.",
+            Input.Validate.REQUIRED);
+
+    public Input<ParametricDistribution> baseDistribInput = new Input<>(
+            "baseDistr",
+            "Base distribution for Dirichlet process",
+            Input.Validate.REQUIRED);
+
+    public Input<Function> scaleParameterInput = new Input<>(
+            "scaleParameter",
+            "Scale parameter for Dirichlet process",
+            Input.Validate.REQUIRED);
+
+    RealParameter param;
+    Function scaleParam;
+    ParametricDistribution distr;
+
+    @Override
+    public void initAndValidate() {
+        param = parameterInput.get();
+        distr = baseDistribInput.get();
+        scaleParam = scaleParameterInput.get();
+    }
+
+    Map<Double,Integer> counts = new HashMap<>();
+
+    @Override
+    public double proposal() {
+
+        double logHR = 0.0;
+
+        int N = param.getDimension();
+        double alpha = scaleParam.getArrayValue();
+
+        int idx = Randomizer.nextInt(N);
+
+        counts.clear();
+        for (int i=0; i<N; i++) {
+            if (i==idx)
+                continue;
+            counts.merge(param.getValue(i), 1, Integer::sum);
+        }
+
+        if (counts.get(param.getValue(idx)) == null)
+            logHR += Math.log(alpha/(alpha+N-1)) + distr.logDensity(param.getValue(idx));
+        else
+            logHR += Math.log(counts.get(param.getValue(idx))/(alpha+N-1));
+
+        double u = Randomizer.nextDouble()*(alpha+N-1);
+        if (u < alpha) {
+            logHR -= Math.log(alpha/(alpha+N-1));
+            try {
+                param.setValue(idx, distr.sample(1)[0][0]);
+                logHR -= distr.logDensity(param.getValue(idx));
+            } catch (MathException e) {
+                throw new RuntimeException("Base distribution does not permit sampling.");
+            }
+        } else {
+            int idxPrime = (int)Math.round(Math.floor(u-alpha));
+
+            if (idxPrime>=idx)
+                idxPrime += 1;
+
+            param.setValue(idx, param.getValue(idxPrime));
+            logHR -= Math.log(counts.get(param.getValue(idxPrime))/(alpha+N-1));
+        }
+
+        return logHR;
+    }
+
+    @Override
+    public List<StateNode> listStateNodes() {
+        List<StateNode> stateNodes = new ArrayList<>();
+        stateNodes.add(param);
+
+        return stateNodes;
+    }
+}
---
+++ src/feast/modelselect/DirichletProcessPrior.java
@@ -0,0 +1,75 @@
+package feast.modelselect;
+
+import beast.core.Distribution;
+import beast.core.Function;
+import beast.core.Input;
+import beast.core.State;
+import beast.math.distributions.ParametricDistribution;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class DirichletProcessPrior extends Distribution {
+
+    public Input<Function> parameterInput = new Input<>("parameter",
+            "Array to which to apply DPP.",
+            Input.Validate.REQUIRED);
+
+    public Input<ParametricDistribution> baseDistribInput = new Input<>(
+            "baseDistr",
+            "Base distribution for Dirichlet process",
+            Input.Validate.REQUIRED);
+
+    public Input<Function> scaleParameterInput = new Input<>("scaleParameter",
+            "Scale parameter of DPP.",
+            Input.Validate.REQUIRED);
+
+    Function parameter, scaleParameter;
+    ParametricDistribution baseDistr;
+
+    @Override
+    public void initAndValidate() {
+        parameter = parameterInput.get();
+        scaleParameter = scaleParameterInput.get();
+        baseDistr = baseDistribInput.get();
+    }
+
+    Map<Double,Integer> counts = new HashMap<>();
+
+    @Override
+    public double calculateLogP() {
+        logP = 0.0;
+
+        double alpha = scaleParameter.getArrayValue();
+
+        counts.clear();
+        for (int i=0; i<parameter.getDimension(); i++) {
+            double x = parameter.getArrayValue(i);
+            if (counts.get(x) != null)
+                logP += Math.log(counts.get(x)/(i+alpha));
+            else
+                logP += Math.log(alpha/(i+alpha)) + baseDistr.logDensity(x);
+
+            counts.merge(x, 1, Integer::sum);
+        }
+
+        return logP;
+    }
+
+    @Override
+    public List<String> getArguments() {
+        return null;
+    }
+
+    @Override
+    public List<String> getConditions() {
+        return null;
+    }
+
+    @Override
+    public void sample(State state, Random random) {
+        throw new UnsupportedOperationException();
+    }
+}
