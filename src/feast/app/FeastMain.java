2b9471c4bce53b9b41c7fcdb054e22d85df40b21
Tim Vaughan <timv@ughan.xyz>
2023-04-13 22:10:18 +0200

    Updated license headers.

--- src/feast/app/FeastMain.java
+++ src/feast/app/FeastMain.java
@@ -1,9 +1,27 @@
+/*
+ * Copyright (c) 2023 Tim Vaughan
+ *
+ * This file is part of feast.
+ *
+ * feast is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * feast is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with feast. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package feast.app;
 
 import beast.pkgmgmt.BEASTClassLoader;
 import beast.pkgmgmt.PackageManager;
 import beastfx.app.beast.BeastMain;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.common.fields.FieldEnumSelector;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -16,8 +34,6 @@ import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.stream.Stream;
 
 public class FeastMain {
--- src/feast/nexus/NexusWriter.java
+++ src/feast/nexus/NexusWriter.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2014 Tim Vaughan <tgvaughan@gmail.com>.
+ * Copyright (c) 2023 Tim Vaughan
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This file is part of feast.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * feast is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
+ * feast is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with feast. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package feast.nexus;
