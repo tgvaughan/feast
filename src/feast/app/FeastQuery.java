ef1a75770b98a1a36c4b8bdd6715cddaebd7d4c2
Tim Vaughan <timothy.vaughan@bsse.ethz.ch>
2024-07-17 14:24:58 +0200

    Broadened set of exceptions caught when probing objects.

--- src/feast/app/FeastQuery.java
+++ src/feast/app/FeastQuery.java
@@ -43,6 +43,7 @@ import org.w3c.dom.events.EventTarget;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.*;
@@ -425,8 +426,7 @@ public class FeastQuery extends Application {
                     inputInfos.add(new InputInfo(o, field));
                 }
 
-            } catch (NoClassDefFoundError | ClassNotFoundException | InstantiationException |
-                     ClassCastException | IllegalAccessException e) {
+            } catch (Error | Exception e) {
                 loadingError = true;
             }
         }
